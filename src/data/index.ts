7368d50829de264407ccedb8be6fdca2a3b2138a
Razak <moabdulrazak17@gmail.com>
2023-03-19 19:21:22 +0000

    feat(components): add Footer

---
+++ src/data/index.ts
@@ -0,0 +1,37 @@
+export const navLinks = [
+	{
+		name: 'Home',
+		url: '#'
+	},
+	{
+		name: 'About',
+		url: '#about'
+	},
+	{
+		name: 'Projects',
+		url: '#projects'
+	},
+	{
+		name: 'Contact',
+		url: '#contact'
+	}
+];
+
+export const footerLinks = [
+	{
+		name: 'Blog',
+		url: 'https://blog.razakmo.me'
+	},
+	{
+		name: 'Projects',
+		url: '#projects'
+	},
+	{
+		name: 'Imprint',
+		url: '#'
+	},
+	{
+		name: 'Privacy Policy',
+		url: '#'
+	}
+];
